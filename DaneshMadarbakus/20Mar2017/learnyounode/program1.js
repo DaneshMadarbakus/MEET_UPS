808036d173ddbb38ad0a4c1b4073c5135a1eb129
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-03-20 20:25:26 +0000

    half way through class

---
+++ DaneshMadarbakus/20Mar2017/learnyounode/program1.js
@@ -0,0 +1,24 @@
+
+
+// 
+// var fs = require('fs');
+// var buffer = fs.
+
+
+// var output = fs.readFileSync(process.argv[2]);
+// var result = output.toString();
+//
+// // console.log(result);
+// console.log(result.split('\n').length - 1);
+
+
+//
+// let sum = 0;
+//
+// // sum = Number(process.argv[2]) + Nuåmber(process.argv[3]) + Number(process.argv[4]);
+//
+// for (let i = 2; i < process.argv.length; i++) {
+//   sum += Number(process.argv[i]);
+// }
+//
+// console.log(sum);
